567065246bf01d9b866b80116d45bc2db6d41a6a
Stepashka20 <stepa.davydov.2002@mail.ru>
2022-12-02 22:33:46 +0300

    Added calculation reward and time limits

--- app/models/gameModel.js
+++ app/models/gameModel.js
@@ -8,20 +8,24 @@ const createGame = async (req,gameParams,field) => {
     const user = await users.findOne({username:decodedToken.username});
 
     const games = req.mongo.collection('games');
+    const reward = calcRewardTime(gameParams.difficulty,gameParams.size,+(gameParams.timeBet.replace('time_','')))
     const game = {
         mode: gameParams.mode,
         field: field,
         userField: [...Array(field.length).keys()].map(i => -2),
         size: gameParams.size,
         difficulty: gameParams.difficulty,
-        reward: 100, //TODO calculate reward
+        reward: {
+            bombs: reward.bombs,
+            stars: 0
+        },
         players: [{
             username: user.username,
             avatar: user.avatar,
             rating: user.rating,
             points: 0,
         }],
-        timeBet: 600, //TODO calculate timeBet
+        timeBet: reward.time, 
         timeStart: Date.now(),
         status: 'waiting',
         uid: randString(10)
@@ -51,58 +55,57 @@ const getGame = async (req,uid) => {
 
 const generateField = (size,difficulty) => {
     let field = [];
-    let mines = 0;
     let minesCount = 0;
     let cellsCount = 0;
     switch (difficulty) {
         case "easy":
-            mines = Math.floor(size * size * 0.1);
+            minesCount = Math.floor(size * size * 0.1);
             break;
         case "medium":
-            mines = Math.floor(size * size * 0.15);
+            minesCount = Math.floor(size * size * 0.15);
             break;
         case "hard":
-            mines = Math.floor(size * size * 0.2);
+            minesCount = Math.floor(size * size * 0.2);
             break;
     }
     cellsCount = size * size;
-    minesCount = mines;
     for (let i = 0; i < cellsCount; i++) {
         field.push(0);
     }
-    for (let i = 0; i < mines; i++) {
+    let placedMinesCount = 0;
+    while (placedMinesCount != minesCount) {
         let rnd = Math.floor(Math.random() * cellsCount);
         if (field[rnd] == 0) {
             field[rnd] = -1;
-        } else {
-            i--;
+            placedMinesCount++;
         }
     }
     for (let i = 0; i < cellsCount; i++) {
         let minesAround = 0;
+        const [x,y] = [i % size,Math.floor(i / size)];
         if (field[i] == 0) {
-            if (i + 1 < cellsCount && field[i + 1] == -1) {
+            if (x > 0 && field[i-1] == -1) {
                 minesAround++;
             }
-            if (i - 1 >= 0 && field[i - 1] == -1) {
+            if (x < size - 1 && field[i+1] == -1) {
                 minesAround++;
             }
-            if (i + size < cellsCount && field[i + size] == -1) {
+            if (y > 0 && field[i-size] == -1) {
                 minesAround++;
             }
-            if (i - size >= 0 && field[i - size] == -1) {
+            if (y < size - 1 && field[i+size] == -1) {
                 minesAround++;
             }
-            if (i + size + 1 < cellsCount && field[i + size + 1] == -1) {
+            if (x > 0 && y > 0 && field[i-size-1] == -1) {
                 minesAround++;
             }
-            if (i + size - 1 < cellsCount && field[i + size - 1] == -1) {
+            if (x > 0 && y < size - 1 && field[i+size-1] == -1) {
                 minesAround++;
             }
-            if (i - size + 1 >= 0 && field[i - size + 1] == -1) {
+            if (x < size - 1 && y > 0 && field[i-size+1] == -1) {
                 minesAround++;
             }
-            if (i - size - 1 >= 0 && field[i - size - 1] == -1) {
+            if (x < size - 1 && y < size - 1 && field[i+size+1] == -1) {
                 minesAround++;
             }
             field[i] = minesAround;
@@ -116,6 +119,23 @@ const randString = (length) => {
     var result = '';
     for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
     return result;
+}
+
+
+const calcRewardTime = (difficulty,size,timeBet) => {
+    const p = {"easy": 1,"medium": 2,"hard": 3}
+    const max_b = 450
+    const max_t = {"easy": 100,"medium": 150,"hard": 200}[difficulty]
+    const b = max_b / 9
+    const t = max_t / 5
+    const pointsMin = b*(3*(size/5-2)+p[difficulty]);
+    const timeMin = t*(size/5-2+4-p[difficulty]);
+
+    return {
+        bombs: [Math.floor(pointsMin*1.9*1.9),Math.floor(pointsMin*1.8),pointsMin][timeBet],
+        time: [timeMin,timeMin*2,timeMin*3][timeBet]
+    }
+    
 }
 
 module.exports = {
